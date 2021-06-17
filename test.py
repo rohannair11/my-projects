429ffdda9cc020bf72a90bb03e253948672da129
rohannair11 <42870265+rohannair11@users.noreply.github.com>
2021-06-17 10:50:53 +0530

    8 ball
    first pr try

---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+
+predictions = ['Definitely','Maybe','I wouldnt worry about it', 'Never', 'No','Ofcourse not who tf are you?']
+choice = random.choice(predictions)
+pred = input("Enter your task = ")
+print(choice)
